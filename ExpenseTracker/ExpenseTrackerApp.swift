58b5bba6a26ee43ce1e1ef7e79af2dc4055f18b6
Broderick E <broderickdejonge@everjonge.com>
2025-02-26 22:02:56 -0500

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Broderick Everitt-deJonge on 2025-02-26.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
